c99b180a5615cdd8ef60769ccd01976854e5851e
Anna Langner <annalangner06@gmail.com>
2020-02-27 22:04:08 +0100

    Changes in the dispaly of elements
    -Adding errors functions,
    -Hiding results, loading and unit elements;

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-document.getElementById('celsius').addEventListener('click', convertCelsiusToFahrenheit);
+document.getElementById('celsius').addEventListener('click', function(e) {
+  document.getElementById('result').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';  
+  setTimeout(convertCelsiusToFahrenheit, 2000);
 
-function convertCelsiusToFahrenheit(e) {
+  e.preventDefault();
+});
+
+function convertCelsiusToFahrenheit() {
   const celsius = document.getElementById('degree');
   const result = document.getElementById('convertedDegree');
 
@@ -10,16 +16,25 @@ function convertCelsiusToFahrenheit(e) {
 
   if(isFinite(resultCelsiusToFahrenheit)) {
     result.value = resultCelsiusToFahrenheit.toFixed(2);
-  } else {
-    console.log('Pleas entry correct number')
-  }
 
-  e.preventDefault();
+    document.getElementById('fahrenheitUnit').style.display = 'none';
+    document.getElementById('celsiusUnit').style.display = 'block';
+    document.getElementById('result').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
+  } else {
+    showError('Pleas entry correct number');
+  }  
 }
 
-document.getElementById('fahrenheit').addEventListener('click', convertFahrenheitToCelsius);
+document.getElementById('fahrenheit').addEventListener('click', function(e) {  
+  document.getElementById('result').style.display = 'none';
+  document.getElementById('loading').style.display = 'block'; 
+  setTimeout( convertFahrenheitToCelsius, 2000);
 
-function convertFahrenheitToCelsius(e) {
+  e.preventDefault();
+});
+
+function convertFahrenheitToCelsius() {
   const fahrenheit = document.getElementById('degree');
   const result = document.getElementById('convertedDegree');
 
@@ -29,9 +44,30 @@ function convertFahrenheitToCelsius(e) {
 
   if(isFinite(resultFahrenheitToCelsius)) {
     result.value = resultFahrenheitToCelsius.toFixed(2);
-  } else {
-    console.log('Pleas entry correct number');
-  }
 
-  e.preventDefault();
+    document.getElementById('celsiusUnit').style.display = 'none';
+    document.getElementById('fahrenheitUnit').style.display = 'block';
+    document.getElementById('result').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
+  } else {
+    showError('Pleas entry correct number');
+  }
+}
+
+function showError(error) {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('result').style.display = 'none';
+
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+  const errorDiv = document.createElement('div');
+  errorDiv.className = 'alert alert-danger';
+  errorDiv.appendChild(document.createTextNode(error));
+
+  card.insertBefore(errorDiv, heading);
+  setTimeout(clearError, 3000);
+}
+
+function clearError(){
+  document.querySelector('.alert').remove();
 }
